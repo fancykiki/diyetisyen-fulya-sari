d13d3d0b41cbd66d9e665f7da5ca324025dff9e5
Fulya Sari <fulya@diyetisyenfulyasari.com>
2026-06-13 11:42:28 +0300

    Fix Vercel deployment - api/index.js self-contained Express app

--- api/index.js
+++ api/index.js
@@ -1,2 +1,25 @@
-const app = require('../server');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const root = path.resolve(__dirname, '..');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(root, 'views'));
+
+app.use(express.static(path.join(root, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', require(path.join(root, 'routes/pages')));
+
+app.use((req, res) => {
+  const siteData = require(path.join(root, 'data/site-data.json'));
+  res.status(404).render('404', {
+    site: siteData.site,
+    path: req.path,
+    meta: { title: 'Sayfa Bulunamadı' }
+  });
+});
+
 module.exports = app;
